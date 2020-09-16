87f36db56e55fecb1901b7fba536fa10fd29c319
Andy Holt <andyholt85@gmail.com>
2020-09-15 23:39:46 -0600

    build desktop-friendly dropdown layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import { defaultObject } from "./state";
+function showDropdown(button_index) {
+  document.getElementById(`dropdown-${button_index}`).classList.add("show");
+}
 
-// This is webpack's default entry file.
+function hideDropdown(button_index) {
+  document.getElementById(`dropdown-${button_index}`).classList.remove("show");
+}
+
+for (let i = 0; i < 5; i++) {
+  btn = document.getElementById(`navbar-item-${i}`);
+  btn.addEventListener("mouseover", function () {
+    showDropdown(i);
+  });
+  btn.addEventListener("mouseout", function () {
+    hideDropdown(i);
+  });
+}
